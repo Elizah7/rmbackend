d34f08087d692bd0df738531c501d5bfbf15373c
Uddeshy Manoj Jaiswal <uddeshkrrish@gmail.com>
2023-05-25 16:16:05 +0530

    newserver

---
+++ index.js
@@ -0,0 +1,8 @@
+const jsonserver = require("json-server");
+const server = jsonserver.create();
+const router  = jsonserver.router("db.json");
+const middleware = jsonserver.defaults();
+const port = 8000;
+server.use(middleware)
+server.use(router)
+server.listen(port)
